e5fcb1459b92ebd98490fdf78f8b21d1e86826b1
MarijnRoothooft <115142201+MarijnRoothooft@users.noreply.github.com>
2025-04-20 14:36:48 +0200

    commit

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { IPersoon } from '../../models/persoon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatCardModule],
+  imports: [MatCardModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
